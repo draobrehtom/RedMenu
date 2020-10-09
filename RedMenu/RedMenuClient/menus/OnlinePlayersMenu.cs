a08e73fb4399d31fc6cbda660618a949c7bc3da6
kibook <kibuzhj@gmail.com>
2020-10-08 21:58:45 -0400

    Fix Online Players teleport

--- RedMenu/RedMenuClient/menus/OnlinePlayersMenu.cs
+++ RedMenu/RedMenuClient/menus/OnlinePlayersMenu.cs
@@ -18,6 +18,7 @@ namespace RedMenuClient.menus
     {
         private static Menu menu = new Menu("Online Players", "Players in the server");
         private static bool setupDone = false;
+        private static List<int> players = new List<int>();
 
         private static int GetTeleportTarget()
         {
@@ -39,32 +40,36 @@ namespace RedMenuClient.menus
             }
         }
 
+        private static void DoOnItemSelect(Menu m, MenuItem item, int index)
+        {
+            int ped = GetTeleportTarget();
+            int target = GetPlayerPed(players[index]);
+            Vector3 coords = GetEntityCoords(target, true, true);
+            float h = GetEntityHeading(target);
+            FreezeEntityPosition(ped, true);
+            SetEntityCoords(ped, coords.X, coords.Y, coords.Z, false, false, false, false);
+            SetEntityHeading(ped, h);
+            FreezeEntityPosition(ped, false);
+        }
+
         private static void SetupMenu()
         {
             if (setupDone) return;
             setupDone = true;
 
-            var players = API.GetActivePlayers();
+            players.Clear();
+            var p = API.GetActivePlayers();
 
-            foreach (var player in players)
+            foreach (var player in p)
             {
                 int id = GetPlayerServerId(player);
                 string name = GetPlayerName(player);
                 MenuItem item = new MenuItem(id.ToString() + ": " + name);
                 menu.AddMenuItem(item);
+                players.Add(player);
             }
 
-            menu.OnItemSelect += (m, item, index) =>
-            {
-                int ped = GetTeleportTarget();
-                int target = GetPlayerPed(players[index]);
-                Vector3 coords = GetEntityCoords(target, true, true);
-                float h = GetEntityHeading(target);
-                FreezeEntityPosition(ped, true);
-                SetEntityCoords(ped, coords.X, coords.Y, coords.Z, false, false, false, false);
-                SetEntityHeading(ped, h);
-                FreezeEntityPosition(ped, false);
-            };
+            menu.OnItemSelect += DoOnItemSelect;
 
             menu.OnMenuOpen += (m) =>
             {
@@ -80,6 +85,7 @@ namespace RedMenuClient.menus
         public static void UpdatePlayerlist()
         {
             menu.ClearMenuItems();
+            menu.OnItemSelect -= DoOnItemSelect;
             setupDone = false;
             SetupMenu();
         }
